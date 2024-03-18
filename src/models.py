cf5b98c0a12f6950798aa70307961491017947f6
M3 <34512995+Grublux@users.noreply.github.com>
2024-03-18 15:42:46 +0000

    star wars data modeling commit

--- src/models.py
+++ src/models.py
@@ -7,23 +7,44 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table user
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    favorites = relationship("Favorites", ForeignKey('favorites.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favorites(Base):
+    __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    fav_by_id = Column(Integer, ForeignKey('user.id'))
+    character = Column(Integer, ForeignKey('character.id'))
+    habitate = Column(Integer, ForeignKey('habitat.id'))
+
+class Character(Base):
+    __tablename__ = 'character'
+    id=Column(Integer, primary_key=True)
+    char_name = Column(String(250), nullable=False)
+    char_description = Column(String(250), nullable=False)
+    char_habitat = Column(Integer, ForeignKey('habitat.id'))
+    char_img_url = Column(String(250), nullable=True)
+
+class Habitat(Base):
+    __tablename__ = 'habitat'
+    id=Column(Integer, primary_key=True)
+    habitat_name = Column(String(250), nullable=False)
+    habitat_description = Column(String(250), nullable=False)
+    inhabitants = Column(Integer, ForeignKey('character.id'))
+
+
+
+
+
+
 
     def to_dict(self):
         return {}
